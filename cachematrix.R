c48a8768e57ff260334d9840c29b8ac3d3f8711a
xnatsux <wielnt@yahoo.com>
2014-09-21 17:39:33 -0400

    Final Answer Functions
    Revised the .R file to include my versions of the functions that are
    consistent with the assignment’s requirements

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions, makeCacheMatrix and cacheSolve
+## these functions are desigend to cache a matrix that is a function input to makeCacheMatrix
+## and to invert the matrix if it has not been previously inverted
 
-## Write a short comment describing this function
+## First is the makeCacheMatrix function, which creates a special output that can be fed to cacheSolve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Second is the cacheSolve function, which checks if the function has been inverted and 
+## calls the cached version if it has been previously inverted (or inverts otherwise)
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
